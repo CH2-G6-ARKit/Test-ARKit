8a3c543d3223ff92c742d3f44e45abade7205bf7
stephan <ardistephan@gmail.com>
2025-05-14 10:39:43 +0800

    Initial Commit

---
+++ Test-ARKit/Test_ARKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ARKitApp.swift
+//  Test-ARKit
+//
+//  Created by stephan on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Test_ARKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test-ARKitTests/Test_ARKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ARKitTests.swift
+//  Test-ARKitTests
+//
+//  Created by stephan on 14/05/25.
+//
+
+import Testing
+@testable import Test_ARKit
+
+struct Test_ARKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
